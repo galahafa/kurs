faa286dafe4c6402694394150220a56d04dc06be
galahafa <39200254+galahafa@users.noreply.github.com>
2018-06-15 13:20:18 +0500

    Add files via upload

---
+++ clock.f90
@@ -0,0 +1,14 @@
+subroutine init_random_seed()
+  integer :: i, n, clock
+  integer, dimension(:), allocatable :: seed
+
+  call random_seed(size = n)
+  allocate(seed(n))
+
+  call system_clock(count=clock)
+
+  seed = clock + 37 * (/ (i - 1, i = 1, n) /)
+  call random_seed(put = seed)
+
+  deallocate(seed)
+end subroutine init_random_seed
